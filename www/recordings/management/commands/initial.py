4af13ebe8549a265685693e8d5101fa76bae65b7
Victor <vykanton@hotmail.com>
2016-11-29 19:12:24 +1300

    Changed the directory of the deployments, organisation, recorders and sites. From fixtures to www/fixtures

--- www/recordings/management/commands/initial.py
+++ www/recordings/management/commands/initial.py
@@ -8,7 +8,7 @@ from django.core.management.base import BaseCommand
 from www.recordings.models import Organisation, Site, Recorder, Deployment
 
 
-DIR = 'fixtures'
+DIR = 'www/fixtures'
 
 
 class Command(BaseCommand):
@@ -33,17 +33,17 @@ class Command(BaseCommand):
         for row in recorders:
             o = Recorder(code=row['Code'], organisation=rfpt_org)
             o.save()
-        
+
         deployments = csv.DictReader(open(os.path.join(DIR, 'Deployments.csv')))
         for row in deployments:
             site = Site.objects.get(code=row['Site'])
             recorder = Recorder.objects.get(code=row['Recorder'].strip())
-            
+
             if not row['Deploy_time']:
                 row['Deploy_time'] = '00:00:00'
             if not row['Recovery_time']:
                 row['Recovery_time'] = '00:00:00'
-            start = datetime.strptime(row['Deploy_date'] + ' ' + row['Deploy_time'], '%d/%m/%Y %H:%M:%S') 
+            start = datetime.strptime(row['Deploy_date'] + ' ' + row['Deploy_time'], '%d/%m/%Y %H:%M:%S')
             if row['Recovery_date'].strip():
                 end = datetime.strptime(row['Recovery_date'] + ' ' + row['Recovery_time'], '%d/%m/%Y %H:%M:%S')
             else:
