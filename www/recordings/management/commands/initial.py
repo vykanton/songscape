1875890cf9b47e7595da14da4995ee4ea6754341
Victor <vykanton@hotmail.com>
2016-11-30 17:29:44 +1300

    Customised initial.py for vuw use case

--- www/recordings/management/commands/initial.py
+++ www/recordings/management/commands/initial.py
@@ -51,18 +51,24 @@ class Command(BaseCommand):
         for row in deployments:
             site = Site.objects.get(code=row['Site'])
             recorder = Recorder.objects.get(code=row['Recorder'].strip())
-
-            if not row['Deploy_time']:
-                row['Deploy_time'] = '00:00:00'
-            if not row['Recovery_time']:
-                row['Recovery_time'] = '00:00:00'
-            start = datetime.strptime(row['Deploy_date'] + ' ' + row['Deploy_time'], '%d/%m/%Y %H:%M:%S')
-            if row['Recovery_date'].strip():
-                end = datetime.strptime(row['Recovery_date'] + ' ' + row['Recovery_time'], '%d/%m/%Y %H:%M:%S')
-            else:
-                end = None
             if ORG == "vuw":
-                o = Deployment(site=site, recorder=recorder, start=start, end=end, comments=row['Comments'], owner=vicu_org)
+                start = row['Start']
+                start = datetime.strptime(start,'%d/%m/%y')
+                end = row['End']
+                end = datetime.strptime(end,'%d/%m/%y')
+            else:
+                if not row['Deploy_time']:
+                    row['Deploy_time'] = '00:00:00'
+                if not row['Recovery_time']:
+                    row['Recovery_time'] = '00:00:00'
+                start = datetime.strptime(row['Deploy_date'] + ' ' + row['Deploy_time'], '%d/%m/%Y %H:%M:%S')
+                if row['Recovery_date'].strip():
+                    end = datetime.strptime(row['Recovery_date'] + ' ' + row['Recovery_time'], '%d/%m/%Y %H:%M:%S')
+                else:
+                    end = None
+
+            if ORG == "vuw":
+                o = Deployment(site=site, recorder=recorder, start=start, end=end, comments='NA', owner=vicu_org)
                 o.save()
             else:
                 o = Deployment(site=site, recorder=recorder, start=start, end=end, comments=row['Comments'], owner=rfpt_org)
