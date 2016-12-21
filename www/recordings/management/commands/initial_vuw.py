676ce5f1c45aeae8f881fb96503ecf81492c5b3b
Victor <vykanton@hotmail.com>
2016-12-21 16:38:57 +1300

    Added analysis and tags information in initial_vuw.py and .csv

---
+++ www/recordings/management/commands/initial_vuw.py
@@ -0,0 +1,55 @@
+import csv
+import os
+import pytz
+
+from datetime import datetime
+
+from django.core.management.base import BaseCommand
+
+from www.recordings.models import Organisation, Site, Recorder, Deployment
+
+from www.settings import ORG
+
+DIR = 'www/fixtures'
+
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        organisation = csv.DictReader(open(os.path.join(DIR, 'Organisations.csv')))
+        for row in organisation:
+            o = Organisation(code=row['Code'], name=row['Name'])
+            o.save()
+
+        vicu_org = Organisation.objects.get(code='VICU')
+
+        sites = csv.DictReader(open(os.path.join(DIR, 'Sites.csv')))
+        for row in sites:
+            if not row['Latitude'].strip():
+                row['Latitude'] = None
+            if not row['Longitude'].strip():
+                row['Longitude'] = None
+
+            o = Site(code=row['Code'], latitude=row['Latitude'], longitude=row['Longitude'], description=row['Comments'], organisation=vicu_org)
+            o.save()
+
+        recorders = csv.DictReader(open(os.path.join(DIR, 'Recorders.csv')))
+        for row in recorders:
+            o = Recorder(code=row['Code'], organisation=vicu_org)
+            o.save()
+
+        deployments = csv.DictReader(open(os.path.join(DIR, 'Deployments.csv')))
+        for row in deployments:
+            site = Site.objects.get(code=row['Site'])
+            recorder = Recorder.objects.get(code=row['Recorder'].strip())
+            start = row['Start']
+            start = pytz.utc.localize(datetime.strptime(start,'%d/%m/%y'))
+            end = row['End']
+            end = pytz.utc.localize(datetime.strptime(end,'%d/%m/%y'))
+            o = Deployment(site=site, recorder=recorder, start=start, end=end, comments='NA', owner=vicu_org, start_timezone='UTC')
+            o.save()
+
+        analysis = csv.DictReader(open(os.path.join(DIR, 'Analysis.csv')))
+        for row in analysis:
+            o = Analysis(name=row['Name_analysis'], code=row['Code_analysis'], description=row['Description_analysis'])
+            o = Tag(code=row['Code_tag'], name=row['Name_tag'])
+            o.save()
