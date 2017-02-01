827685f8542b29b5b6cd3b6b7f9ddcb06e48b186
Victor <vykanton@hotmail.com>
2017-02-01 13:22:14 +1300

    Sample snippets based on score from kiwi detectors

--- www/recordings/management/commands/select_snippets_for_analysis_vuw.py
+++ www/recordings/management/commands/select_snippets_for_analysis_vuw.py
@@ -8,6 +8,22 @@ from www.recordings.models import Snippet, Analysis, AnalysisSet, Detector, Scor
 class Command(BaseCommand):
     def handle(self, *args, **options):
         analysis = Analysis.objects.get(code='hihi_id')
+        deployments = Deployment.objects.all()
+        clipping = Detector.objects.get(code='amplitude')
         snippet = Snippet.objects.all()
-        for snip in snippet:
-            AnalysisSet(analysis=analysis, snippet=snip, selection_method='all').save()
+        code = 'simple-north-island-brown-kiwi'
+        version = '0.1.2'
+        detector = Detector.objects.get(code=code, version=version)
+        for deployment in deployments:
+            snippets = Snippet.objects.\
+                filter(recording__deployment=deployment)
+            already = snippets.filter(sets__analysis=analysis)
+
+            #select by kiwi score
+            hihi_snippets = snippets.filter(scores__detector=detector,
+                scores__score__gt=25).exclude(id__in=already)
+
+            print deployment, len(already), len(hihi_snippets)
+
+            for snip in hihi_snippets:
+                AnalysisSet(analysis=analysis, snippet=snip,selection_method=detector).save()
