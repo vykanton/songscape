a728a2e9d0e3a0fdb6fcf3051d6a103d436d7db1
Jason Preble <jhiikun@gmail.com>
2017-02-20 23:40:45 +0900

    copy_score_files_vuw now includes snippet score

--- www/recordings/management/commands/copy_scored_files_vuw.py
+++ www/recordings/management/commands/copy_scored_files_vuw.py
@@ -30,12 +30,12 @@ class Command(BaseCommand):
         tags = args[1:]
         print ("tags=",tags,"analysis=",analysis)
         identifications = Identification.objects.filter(analysisset__analysis__code=analysis)
-        print ("identifications=",identifications)
+        #print ("identifications=",identifications)
         if user:
             identifications = identifications.filter(user__username=user)
 
         call_labels = CallLabel.objects.filter(analysisset__analysis__code=analysis)
-        print ("call_lables=",call_labels)
+        #print ("call_lables=",call_labels)
         if user:
             call_labels = call_labels.filter(user__username=user)
 
@@ -54,15 +54,15 @@ class Command(BaseCommand):
         calls = {}
         for identification in identifications:
             snippet_score=Score.objects.filter(snippet__id=identification.analysisset.snippet.id).values_list('score',flat=True)[0]
-            print("score1=",snippet_score)
-            #TODO add the score for each snippet
+            #print("score1=",snippet_score)
             calls[identification.analysisset.snippet.get_soundfile_name()] = []
 
         for call in call_labels:
             if call.tag.code in tags:
                 snippet_score=Score.objects.filter(snippet__id=call.analysisset.snippet.id).values_list('score',flat=True)[0]
-                print("score2=",snippet_score)
-                calls[call.analysisset.snippet.get_soundfile_name()].append((standardize(call.start_time),standardize(call.end_time)))
+                #print("score2=",snippet_score)
+                calls[call.analysisset.snippet.get_soundfile_name()].append((standardize(call.start_time),standardize(call.end_time),snippet_score))
+                #print calls
 
         # Now output the data
         output = open('call-labels-%s.txt' % ('-'.join(tags)), 'w')
@@ -77,12 +77,14 @@ class Command(BaseCommand):
                     if label[0] > stack[-1]:
                         stack.append(label[0])
                         stack.append(label[1])
+                        stack.append(label[2])
                     else:
                         stack.pop()
                         stack.append(label[1])
-            if not sorted(stack) == stack:
-                print stack
-                raise ValueError, 'call times should be sorted'
+            #if not sorted(stack) == stack:
+            #    print stack
+            #    raise ValueError, 'call times should be sorted'
+            print stack
             output.write(soundfile + ' ')
             output.write(' '.join([str(s) for s in stack]))
             output.write('\n')
