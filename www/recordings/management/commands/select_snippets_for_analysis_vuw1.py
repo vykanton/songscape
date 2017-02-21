5be9b64559470b3b1c1a74c08ead7a9f3ec146b5
Victor <vykanton@hotmail.com>
2017-02-21 14:03:18 +1300

    select snippets based on kiwi scores

--- www/recordings/management/commands/select_snippets_for_analysis_vuw1.py
+++ www/recordings/management/commands/select_snippets_for_analysis_vuw1.py
@@ -8,21 +8,22 @@ from www.recordings.models import Snippet, Analysis, AnalysisSet, Detector, Scor
 class Command(BaseCommand):
     def handle(self, *args, **options):
         analysis = Analysis.objects.get(code='hihi_id')
+        clipping = Detector.objects.get(code='amplitude')
         snippets = Snippet.objects.all()
-        code = 'hihi'
-        version = '0.0.1'
+        code = 'simple-north-island-brown-kiwi'
+        version = '0.1.2'
         detector = Detector.objects.get(code=code, version=version)
         already = snippets.filter(sets__analysis=analysis)
         score= Score.objects.all()
-        
-        #select snippets by hihi score
+
+        #select snippets by kiwi score
         #max_score=max(score)
-        max_score=20
-        threshold_score=1
-        category_length=3
+        max_score=40
+        threshold_score=25
+        category_length=5
         score_categories=range(threshold_score,max_score+category_length,category_length)
         #number of snippets per category
-        snippet_category=2
+        snippet_category=3
         hihi_snippets = snippets.filter(scores__detector=detector,
             scores__score__gt=threshold_score).exclude(id__in=already)
         #Now select random snippets within each score category
