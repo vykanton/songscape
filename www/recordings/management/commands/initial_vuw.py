f958fa7dd436e21f7598b69910468ab573dfcb24
andre-geldenhuis <andre.geldenhuis@vuw.ac.nz>
2017-03-02 14:00:09 +1300

    initial_vuw can now be run multiple times without error

--- www/recordings/management/commands/initial_vuw.py
+++ www/recordings/management/commands/initial_vuw.py
@@ -17,8 +17,11 @@ class Command(BaseCommand):
     def handle(self, **options):
         organisation = csv.DictReader(open(os.path.join(DIR, 'Organisations.csv')))
         for row in organisation:
-            o = Organisation(code=row['Code'], name=row['Name'])
-            o.save()
+            #check if organisation already exists
+            org_exists = Organisation.objects.filter(code=row['Code']).exists()
+            if ~org_exists:
+                o = Organisation(code=row['Code'], name=row['Name'])
+                o.save()
 
         vicu_org = Organisation.objects.get(code='VICU')
 
@@ -29,13 +32,33 @@ class Command(BaseCommand):
             if not row['Longitude'].strip():
                 row['Longitude'] = None
 
-            o = Site(code=row['Code'], latitude=row['Latitude'], longitude=row['Longitude'], description=row['Comments'], organisation=vicu_org)
-            o.save()
+            site_exists = Site.objects.filter(
+                    code=row['Code']
+                ).filter(
+                    latitude=row['Latitude']
+                ).filter(
+                    longitude=row['Longitude']
+                ).filter(
+                     description=row['Comments']
+                ).filter(
+                     organisation=vicu_org
+                ).exists()
+
+            if ~site_exists:
+                o = Site(code=row['Code'], latitude=row['Latitude'], longitude=row['Longitude'], description=row['Comments'], organisation=vicu_org)
+                o.save()
 
         recorders = csv.DictReader(open(os.path.join(DIR, 'Recorders.csv')))
         for row in recorders:
-            o = Recorder(code=row['Code'], organisation=vicu_org)
-            o.save()
+
+            recorder_exists = Recorder.objects.filter(
+                code=row['Code']
+            ).filter(
+                organisation=vicu_org
+            ).exists()
+            if ~recorder_exists:
+                o = Recorder(code=row['Code'], organisation=vicu_org)
+                o.save()
 
         deployments = csv.DictReader(open(os.path.join(DIR, 'Deployments.csv')))
         for row in deployments:
@@ -45,15 +68,49 @@ class Command(BaseCommand):
             start = pytz.utc.localize(datetime.strptime(start,'%d/%m/%y'))
             end = row['End']
             end = pytz.utc.localize(datetime.strptime(end,'%d/%m/%y'))
-            o = Deployment(site=site, recorder=recorder, start=start, end=end, comments='NA', owner=vicu_org, start_timezone='UTC')
-            o.save()
+
+            deployment_exists = Deployment.objects.filter(
+                    site=site
+                ).filter(
+                    recorder=recorder
+                ).filter(
+                    start=start
+                ).filter(
+                    end=end
+                ).filter(
+                    comments='NA'
+                ).filter(
+                    owner=vicu_org
+                ).filter(
+                    start_timezone='UTC'
+            ).exists()
+
+            if ~deployment_exists:
+                o = Deployment(site=site, recorder=recorder, start=start, end=end, comments='NA', owner=vicu_org, start_timezone='UTC')
+                o.save()
 
         tags = csv.DictReader(open(os.path.join(DIR, 'Tags.csv')))
         for row in tags:
-            o = Tag(code=row['Code'], name=row['Name'])
-            o.save()
+            tag_exists = Tag.object.filter(
+                code=row['Code']
+            ).filter(
+                name=row['Name']
+            ).exists()
+            if ~tag_exists:
+                o = Tag(code=row['Code'], name=row['Name'])
+                o.save()
 
         analysis = csv.DictReader(open(os.path.join(DIR, 'Analysis.csv')))
         for row in analysis:
-            o = Analysis(name=row['Name'], code=row['Code'], description=row['Description'], user_id=row['User_id'])
-            o.save()
+            analysis_exists = Analysis.objects.filter(
+                name=row['Name']
+            ).filter(
+                code=row['Code']
+            ).filter(
+                description=row['Description']
+            ).filter(
+                user_id=row['User_id']
+            )
+            if ~analysis_exists:
+                o = Analysis(name=row['Name'], code=row['Code'], description=row['Description'], user_id=row['User_id'])
+                o.save()
