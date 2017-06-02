3945260ebd57642084cd71dd12af50444f5f7140
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2017-06-02 18:15:57 +1200

    Fixed bug were recordings were not correctly divided between CPU's

--- www/recordings/management/commands/score_snippets_vuw.py
+++ www/recordings/management/commands/score_snippets_vuw.py
@@ -19,6 +19,7 @@ from www.recordings.models import Score, Recording, Snippet, Detector
 from www.settings import DETECTOR_CORES, HIHI_DETECTOR, KAKARIKI_DETECTOR, TIEKE_DETECTOR
 
 import multiprocessing as mp
+import numpy as np
 
 def worker(cpu_recording_ids, detector_id):
     '''
@@ -28,55 +29,61 @@ def worker(cpu_recording_ids, detector_id):
     or from spawning processes
     '''
 
-    # create new database connections for this worker.
-    recordings = Recording.objects.filter(pk__in = cpu_recording_ids)
-    detector_model = Detector.objects.get(pk = detector_id)
+    #Check that this CPU actually has recordigns to processes
+    if len(cpu_recording_ids>0):
+        
+        # create new database connections for this worker.
+        recordings = Recording.objects.filter(pk__in = cpu_recording_ids)
+        detector_model = Detector.objects.get(pk = detector_id)
 
-    # Reinitialise detectors for multiprocessing - TODO this could be done in a cleaner way.
-    if detector_model.code == 'hihi':
-        detector = HihiCNN(HIHI_DETECTOR, prediction_block_size=10, num_cores = DETECTOR_CORES)
-    elif detector_model.code == 'kakariki':
-        detector = KakarikiRNN(KAKARIKI_DETECTOR, num_cores = DETECTOR_CORES)
-    elif detector_model.code == 'tieke':
-        detector = TiekeRNN(TIEKE_DETECTOR, num_cores = DETECTOR_CORES)
-    else:
-        raise ValueError('Unknown detector code! - Make sure new detectors are added to code here!')
-        detector = None
-
-
-
-    now = time.time()
-
-    for recording in recordings:
-        snippets = Snippet.objects.filter(recording=recording).exclude(scores__detector=detector_model).order_by('offset')
-        if len(snippets):
-            for snippet in snippets:
-                try:
-                    audio = Audio(*get_audio(recording.path, snippet.offset, snippet.duration))
-                    count = 0
-                    score = detector.score(audio)
-                    if not count:
-                        print '%s %0.1f %0.1f' % (snippet, time.time() - now, score)
-                        now = time.time()
-                    try:
-                        s = Score.objects.get(detector=detector_model, snippet=snippet)
-                        s.delete()
-                    except Score.DoesNotExist:
-                        pass
-                    s = Score(detector=detector_model, snippet=snippet,
-                        score=score)
-                    s.save()
-                    count += 1
-                except KeyboardInterrupt:
-                    raise
-                except WaveError:
-                    print recording.path, 'scoring failed because of a WAV error'
-                    break
-                except:
-                    print detector, snippet, 'Scoring failed', sys.exc_info()[0]
+        # Reinitialise detectors for multiprocessing - TODO this could be done in a cleaner way.
+        if detector_model.code == 'hihi':
+            detector = HihiCNN(HIHI_DETECTOR, prediction_block_size=10, num_cores = DETECTOR_CORES)
+        elif detector_model.code == 'kakariki':
+            detector = KakarikiRNN(KAKARIKI_DETECTOR, num_cores = DETECTOR_CORES)
+        elif detector_model.code == 'tieke':
+            detector = TiekeRNN(TIEKE_DETECTOR, num_cores = DETECTOR_CORES)
         else:
-            raise ValueError('There are no matching snippets to the recording in the database!')
-            
+            raise ValueError('Unknown detector code! - Make sure new detectors are added to code here!')
+            detector = None
+
+
+
+        now = time.time()
+
+        for recording in recordings:
+            snippets = Snippet.objects.filter(recording=recording).exclude(scores__detector=detector_model).order_by('offset')
+            if len(snippets):
+                for snippet in snippets:
+                    try:
+                        audio = Audio(*get_audio(recording.path, snippet.offset, snippet.duration))
+                        count = 0
+                        score = detector.score(audio)
+                        if not count:
+                            print '%s %0.1f %0.1f' % (snippet, time.time() - now, score)
+                            now = time.time()
+                        try:
+                            s = Score.objects.get(detector=detector_model, snippet=snippet)
+                            s.delete()
+                        except Score.DoesNotExist:
+                            pass
+                        s = Score(detector=detector_model, snippet=snippet,
+                            score=score)
+                        s.save()
+                        count += 1
+                    except KeyboardInterrupt:
+                        raise
+                    except WaveError:
+                        print recording.path, 'scoring failed because of a WAV error'
+                        break
+                    except:
+                        print detector, snippet, 'Scoring failed', sys.exc_info()[0]
+            else:
+                raise ValueError('There are no matching snippets to the recording in the database!')
+    else:
+        #no recordings to process
+        pass
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -94,22 +101,15 @@ class Command(BaseCommand):
 
         l = recording_ids
         cpus = mp.cpu_count()
-        n = cpus-1 # as we want the remainder of jobs in the last CPU. FIXME cases where it divides evenly
 
-        #Divide recordings as evenly as possible betwen cpus. A nested list, each sublist is the recording ids for a cpu.
-        recordings_per_cpu = [l[i:i + n] for i in xrange(0, len(l), n)]
+        #Divide recordings as evenly as possible betwen cpus. A nested array, each subarray is the recording ids for a cpu.
+        recordings_per_cpu = np.array_split(recording_ids,cpus) #If there are more cpus, than recordings this will just create an empty array for that cpu
 
         # close db all db connections before going multicore
         db.connections.close_all()
 
-        if len(recording_ids) < cpus:
-            num_jobs = len(recording_ids)
-            recordings_per_cpu = [[id] for id in recording_ids] #ie one per cpu
-        else:
-            num_jobs = cpus
-
         jobs=[]
-        for cpu in range(num_jobs):
+        for cpu in range(cpus):
             p = mp.Process(target=worker, args=(recordings_per_cpu[cpu], detector_id))
             jobs.append(p)
             p.start()
