90cbfc6bab252f00814664fff2a987f5cba84346
root <root@vuwunicohalo001.vuw.ac.nz>
2017-07-13 11:11:48 +1200

    Enabled to get audio based on different detectors

--- www/recordings/management/commands/get_files_for_analysis_set_vuw.py
+++ www/recordings/management/commands/get_files_for_analysis_set_vuw.py
@@ -5,8 +5,11 @@ from django.core.management.base import BaseCommand, CommandError
 from www.recordings.models import Snippet, Analysis, AnalysisSet, Detector, Score
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+	parser.add_argument('code_analysis',type=str)
     def handle(self, *args, **options):
-        analysis = Analysis.objects.get(code='hihi_id')
+        analysis_code=str(options['code_analysis'])+str('_id')
+	analysis = Analysis.objects.get(code=analysis_code)
         snippets = [x.snippet for x in AnalysisSet.objects.filter(analysis=analysis)]
         count = 0
         for snippet in snippets:
