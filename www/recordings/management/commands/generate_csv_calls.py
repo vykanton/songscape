6aa22d5e221865c430cdacab7910e08ab5930136
vykanton <vykanton@hotmail.com>
2017-07-17 09:15:25 +1200

    start/end of calls based on snippet info instead of audio file

--- www/recordings/management/commands/generate_csv_calls.py
+++ www/recordings/management/commands/generate_csv_calls.py
@@ -16,7 +16,7 @@ class Command(BaseCommand):
         csv_file = open(csv_path, 'w')
         print("csv_created")
         writer = csv.writer(csv_file)
-        writer.writerow(['filename','snippet_start','snippet_end','call',"call_start","call_length",'high_frequency','low_frequency',
+        writer.writerow(['filename','snippet_start','snippet_end','call',"call_start","call_end",'high_frequency','low_frequency',
                         'score_tieke','score_hihi','score_kakariki'])
         for call in call_labels:
             snippet_call = call.analysisset.snippet
@@ -26,14 +26,14 @@ class Command(BaseCommand):
             species = call.tag.code
             snippet_start = snippet_call.offset
             snippet_end = snippet_call.offset+60
-            call_start = call.start_time+snippet_start
-            call_length = call.end_time-call.start_time            
+            call_start = call.start_time
+            call_end = call.end_time            
             high_frequency = call.high_frequency
             low_frequency = call.low_frequency
             score_tieke = Score.objects.filter(snippet__id=snippet_id,detector__code='tieke')
             score_hihi = Score.objects.filter(snippet__id=snippet_id,detector__code='hihi')
             score_kakariki = Score.objects.filter(snippet__id=snippet_id,detector__code='kakariki')
-            writer.writerow([filename,snippet_start,snippet_end,species,call_start,call_length,high_frequency,low_frequency,
+            writer.writerow([filename,snippet_start,snippet_end,species,call_start,call_end,high_frequency,low_frequency,
                             score_tieke,score_hihi,score_kakariki])
         
         print('file cereated')                                             
