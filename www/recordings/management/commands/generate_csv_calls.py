7e601a8b6eb2abbad8b843982e28d02fdffb70f1
vykanton <vykanton@hotmail.com>
2017-07-17 08:59:48 +1200

    rearrange the columns

--- www/recordings/management/commands/generate_csv_calls.py
+++ www/recordings/management/commands/generate_csv_calls.py
@@ -16,8 +16,8 @@ class Command(BaseCommand):
         csv_file = open(csv_path, 'w')
         print("csv_created")
         writer = csv.writer(csv_file)
-        writer.writerow(['filename','call_id','call',"call_start","call_length",'high_frequency','low_frequency',
-                        'score_tieke','score_hihi','score_kakariki','snippet_start','snippet_end'])
+        writer.writerow(['filename','snippet_start','snippet_end','call',"call_start","call_length",'high_frequency','low_frequency',
+                        'score_tieke','score_hihi','score_kakariki'])
         for call in call_labels:
             snippet_call = call.analysisset.snippet
             snippet_id = snippet_call.id
@@ -33,7 +33,7 @@ class Command(BaseCommand):
             score_tieke = Score.objects.filter(snippet__id=snippet_id,detector__code='tieke')
             score_hihi = Score.objects.filter(snippet__id=snippet_id,detector__code='hihi')
             score_kakariki = Score.objects.filter(snippet__id=snippet_id,detector__code='kakariki')
-            writer.writerow([filename,call_id,species,call_start,call_length,high_frequency,low_frequency,
-                            score_tieke,score_hihi,score_kakariki,snippet_start,snippet_end])
+            writer.writerow([filename,snippet_start,snippet_end,species,call_start,call_length,high_frequency,low_frequency,
+                            score_tieke,score_hihi,score_kakariki])
         
         print('file cereated')                                             
